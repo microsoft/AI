d8da7ffbd83d05ba155b05b713cea15c840c693a
Daniel Ciborowski <dciborow@microsoft.com>
2019-09-12 10:35:29 -0700

    Set secret (#2)
    * Update realtimeserving-release.yml for Azure Pipelines
    
    * Update realtimeserving-release.yml for Azure Pipelines
    
    * script that will be used to capture storage connection string.
    
    * cleaning up entrypoint and printing out status of adding secret to keyvault

---
+++ .ci/scripts/set_secret.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import argparse
+from azure.keyvault import KeyVaultClient
+from azure.common.client_factory import get_client_from_cli_profile
+
+
+def set_secret(kv_endpoint, secret_name, secret_value):
+    client = get_client_from_cli_profile(KeyVaultClient)
+
+    try:
+        client.set_secret(kv_endpoint, secret_name, secret_value)
+        return "Successfully created secret: {secret_name} in keyvault: {kv_endpoint}".format(
+            secret_name=secret_name, kv_endpoint=kv_endpoint)
+    except Exception as ex:
+        return "Failed to create secret: {secret_name} in keyvault: {kv_endpoint}".format(
+            secret_name=secret_name, kv_endpoint=kv_endpoint)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-n', '--secretName', required=True,
+                        help="The name of the secret")
+    parser.add_argument('-v', '--secretValue', required=True,
+                        help="The value of the secret")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    # hard coded for now
+    kv_endpoint = "https://t3scriptkeyvault.vault.azure.net/"
+    args = parse_args()
+    message = set_secret(
+        kv_endpoint, args.secretName, args.secretValue)
+
+    print(message)
