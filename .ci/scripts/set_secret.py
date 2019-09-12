6aec1698450ae150f1611c4a1491f6c9ae17d57f
Daniel Ciborowski <dciborow@microsoft.com>
2019-09-12 13:04:13 -0700

    Update set_secret.py

--- .ci/scripts/set_secret.py
+++ .ci/scripts/set_secret.py
@@ -3,7 +3,7 @@
 import argparse
 from azure.keyvault import KeyVaultClient
 from azure.common.client_factory import get_client_from_cli_profile
-
+import os
 
 def set_secret(kv_endpoint, secret_name, secret_value):
     client = get_client_from_cli_profile(KeyVaultClient)
@@ -22,17 +22,16 @@ def parse_args():
 
     parser.add_argument('-n', '--secretName', required=True,
                         help="The name of the secret")
-    parser.add_argument('-v', '--secretValue', required=True,
-                        help="The value of the secret")
 
     return parser.parse_args()
 
 
 if __name__ == "__main__":
+       
     # hard coded for now
     kv_endpoint = "https://t3scriptkeyvault.vault.azure.net/"
     args = parse_args()
-    message = set_secret(
-        kv_endpoint, args.secretName, args.secretValue)
+    key = os.getenv("storage_conn_string")
+    message = set_secret(kv_endpoint, args.secretName, key)
 
     print(message)
